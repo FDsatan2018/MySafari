efdd75634a27e3e02cabffe84a41e1f25e09e91c
FDsatan <FDsatan@fdsatandemacbook-air.local>
2018-04-22 01:53:16 +0800

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by FDsatan on 2018/4/22.
+//  Copyright © 2018年 FDsatan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
