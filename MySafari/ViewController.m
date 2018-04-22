058490c0eed167d9e04f75ebeaef765c153ed7ad
FDsatan <FDsatan@fdsatandemacbook-air.local>
2018-04-22 14:56:42 +0800

    添加了一个Label。

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, 200, 100)];
+    label.text = @"Hello World!";
+    label.textColor = [UIColor blackColor];
+    label.font = [UIFont systemFontOfSize:15];
+    
+    [self.view addSubview:label];
 }
 
 
